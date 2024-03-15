eab8ff1316f69cfade3febc87e593563cc1d423a
DillBall <baldwidi@oregonstate.edu>
2024-03-14 20:04:57 -0700

    checking in a few components

--- warehouse-dashboard/src/App.jsx
+++ warehouse-dashboard/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import SignIn from './components/SignOn'
+import SignUp from './components/SignUp'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <SignUp />
     </>
   )
 }
